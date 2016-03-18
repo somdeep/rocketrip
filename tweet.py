0ac5fd10742fe9fe95d97b36b32380a325a28f63
somdeep <sd2988@columbia.edu>
2016-03-18 01:53:23 -0400

    Working version

--- tweet.py
+++ tweet.py
@@ -29,10 +29,17 @@ def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=No
     resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
     return content
  
-home_timeline = oauth_req( 'https://api.twitter.com/1.1/search/tweets.json?q=%22manchester+united%22&count=1&result_type=recent', '619422408-yPaRg0V77PgbJK1ENvrscuhD8KDKTfo675em1uit', 'kGo6TtddMPgdED45ohrkN5tpZa7gB3q7iFrMIdo61UWOb' )
+home_timeline = oauth_req( 'https://api.twitter.com/1.1/search/tweets.json?q=%22'+sys.argv[1]+'%22&count=1&result_type=recent', '619422408-yPaRg0V77PgbJK1ENvrscuhD8KDKTfo675em1uit', 'kGo6TtddMPgdED45ohrkN5tpZa7gB3q7iFrMIdo61UWOb' )
 
 home=json.loads(home_timeline)
+print sys.argv[1]
+
 
 for key in home['statuses'] : 
     print (key['user']['name']),":",(key['text'].encode("utf-8"))
-#          .encode("utf-8"));
+    if 'media' in key['entities']:
+        for i in key['entities']['media']:
+            print 'Media : ',i['media_url']
+
+    else:
+        print 'No media'
